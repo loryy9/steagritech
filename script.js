2ae1e8057b6e9fd2239c0a820e9fc8b1f8c17493
loryy9 <102917754+loryy9@users.noreply.github.com>
2024-02-09 19:09:18 +0100

    beta_2.6
    cambiati loghi aziende, aggiunto popup, sistemati navlinks

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+function openPopup() {
+    var popup = document.getElementById("popup");
+    anime({
+        targets: popup,
+        opacity: [0, 1],
+        easing: 'easeInOutQuad',
+        duration: 300,
+        begin: function () {
+            popup.style.display = 'block';
+        }
+    });
+}
+
+function closePopup() {
+    var popup = document.getElementById("popup");
+    anime({
+        targets: popup,
+        opacity: [1, 0],
+        easing: 'easeInOutQuad',
+        duration: 300,
+        complete: function () {
+            popup.style.display = 'none';
+        }
+    });
+}
+
 function animateColumns() {
     anime({
         targets: '#leftColumn',
@@ -20,7 +46,6 @@ function animateColumns() {
 window.onload = function () {
     animateColumns();
 };
-
 
 document.addEventListener("DOMContentLoaded", function () {
     var numbers = document.querySelectorAll(".number");
@@ -52,6 +77,13 @@ document.addEventListener("DOMContentLoaded", function () {
             delay: anime.stagger(500),
         });
     });
+
+    // Aggiunta della parte per associare la funzione openPopup al click del link
+    var contactLink = document.querySelector(".about-me-button a");
+    if (contactLink) {
+        contactLink.addEventListener("click", function (event) {
+            event.preventDefault();
+            openPopup();
+        });
+    }
 });
-
-
