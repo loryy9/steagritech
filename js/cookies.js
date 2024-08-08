6f60e9dccae1ce98f917f9662be63d81ef140c9b
Lorenzo Bellotti <bellotti.lorenzo14@gmail.com>
2024-08-08 17:04:35 +0200

    sistemato banner cookie

--- js/cookies.js
+++ js/cookies.js
@@ -7,7 +7,7 @@ function setCookie(name, value, days) {
     var expires = "";
     if (days) {
         var date = new Date();
-        date.setTime(date.getTime() + (0 * 24 * 60 * 60 * 1000));
+        date.setTime(date.getTime() + (1 * 24 * 60 * 60 * 1000));
         expires = "; expires=" + date.toUTCString();
     }
     document.cookie = name + "=" + (value || "") + expires + "; path=/";
